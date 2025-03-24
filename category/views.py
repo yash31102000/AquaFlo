76620a3f9f58cd8431aa3d53fe30a81e5ea30c77
Yash <yash31102000.s@gmail.com>
2025-03-24 16:35:17 +0530

    changes

--- category/views.py
+++ category/views.py
@@ -5,7 +5,6 @@ from .models import Category
 from .serializers import *
 
 
-
 class CategoryViewSet(generics.GenericAPIView):
     serializer_class = CategorySerializer
 
@@ -15,14 +14,13 @@ class CategoryViewSet(generics.GenericAPIView):
         if Category.objects.filter(name=request.data.get("name")).exists():
             return Response(
                 {
-                    "status": False,     
+                    "status": False,
                     "message": "Category already exists",
                     "data": {},
                 },
                 status=status.HTTP_200_OK,
             )
 
-        
         # Serialize and validate the request data
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -36,7 +34,7 @@ class CategoryViewSet(generics.GenericAPIView):
                 },
                 status=status.HTTP_201_CREATED,
             )
-        
+
         # Handle errors
         return Response(
             {
@@ -50,7 +48,7 @@ class CategoryViewSet(generics.GenericAPIView):
     # Handle GET request for fetching the list of Categories
     def get(self, request):
         # Fetch categories
-        categories = Category.objects.all().filter(is_deleted = False)
+        categories = Category.objects.all().filter(is_deleted=False)
 
         # Prepare the response structure
         response_data = []
@@ -58,34 +56,36 @@ class CategoryViewSet(generics.GenericAPIView):
         # Iterate over each category to structure the data
         for category in categories:
             category_data = {
-                "id" : category.pk,
-                'main_category_list': category.name,  # Assuming 'name' is the field for main category
-                'sub_category_list': [],
+                "id": category.pk,
+                "main_category_list": category.name,  # Assuming 'name' is the field for main category
+                "sub_category_list": [],
             }
 
             # Get sub-categories related to the category
-            sub_categories = Item.objects.filter(category=category)
+            sub_categories = Item.objects.filter(category=category, is_deleted=False)
 
             for sub_category in sub_categories:
                 sub_category_data = {
-                    "id" : sub_category.pk,
-                    'main_item': sub_category.name,  # Assuming 'name' is the field for sub-category
-                    'sub_item': [],
+                    "id": sub_category.pk,
+                    "main_item": sub_category.name,  # Assuming 'name' is the field for sub-category
+                    "sub_item": [],
                 }
 
                 # Get items related to the sub-category
-                items = SubItem.objects.filter(item=sub_category)
+                items = SubItem.objects.filter(item=sub_category, is_deleted=False)
                 for item in items:
                     item_data = {
-                        "id" : item.pk,
-                        'image': item.image.url if item.image else None,  # Assuming 'image_url' is the field for the image
-                        'name': item.name,  # Assuming 'name' is the field for item name
-                        'count': "",  # Assuming 'count' is the field for item count
+                        "id": item.pk,
+                        "image": (
+                            item.image.url if item.image else None
+                        ),  # Assuming 'image_url' is the field for the image
+                        "name": item.name,  # Assuming 'name' is the field for item name
+                        "count": "",  # Assuming 'count' is the field for item count
                     }
-                    sub_category_data['sub_item'].append(item_data)
+                    sub_category_data["sub_item"].append(item_data)
 
                 # Add sub-category data to the category data
-                category_data['sub_category_list'].append(sub_category_data)
+                category_data["sub_category_list"].append(sub_category_data)
 
             # Add category data to the response list
             response_data.append(category_data)
@@ -100,9 +100,8 @@ class CategoryViewSet(generics.GenericAPIView):
             status=status.HTTP_200_OK,
         )
 
+    def delete(self, request, pk):
 
-    def delete(self, request,pk):
-    
         try:
             admin = Category.objects.get(id=pk, is_deleted=False)
             admin.is_deleted = True
@@ -125,6 +124,7 @@ class CategoryViewSet(generics.GenericAPIView):
                 status=status.HTTP_404_NOT_FOUND,
             )
 
+
 class ItemViewSet(generics.GenericAPIView):
     serializer_class = ItemSerializer
     # permission_classes = [IsAdminUser]  # Use IsAdminUser for admins only, adjust as needed
@@ -140,11 +140,11 @@ class ItemViewSet(generics.GenericAPIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST,  # Use 400 Bad Request when there's a validation issue
             )
-        
+
         # Create a new item if it does not exist
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            
+
             serializer.save()
             return Response(
                 {
@@ -163,7 +163,7 @@ class ItemViewSet(generics.GenericAPIView):
             },
             status=status.HTTP_400_BAD_REQUEST,  # Use 400 Bad Request if the serializer validation fails
         )
-    
+
     # Handle GET request for fetching the list of Items
     def get(self, request):
         queryset = Item.objects.all().filter(is_deleted=False)
@@ -176,9 +176,9 @@ class ItemViewSet(generics.GenericAPIView):
             },
             status=status.HTTP_200_OK,
         )
-    
-    def delete(self, request,pk):
-    
+
+    def delete(self, request, pk):
+
         try:
             admin = Item.objects.get(id=pk, is_deleted=False)
             admin.is_deleted = True
@@ -200,7 +200,8 @@ class ItemViewSet(generics.GenericAPIView):
                 },
                 status=status.HTTP_404_NOT_FOUND,
             )
-        
+
+
 class SubItemViewSet(generics.GenericAPIView):
     serializer_class = SubItemSerializer
     # permission_classes = [IsAdminUser]  # Use IsAdminUser for admins only, adjust as needed
@@ -216,11 +217,11 @@ class SubItemViewSet(generics.GenericAPIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST,  # Use 400 Bad Request when there's a validation issue
             )
-        
+
         # Create a new item if it does not exist
         serializer = SubItemSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            
+
             serializer.save()
             return Response(
                 {
@@ -239,7 +240,7 @@ class SubItemViewSet(generics.GenericAPIView):
             },
             status=status.HTTP_400_BAD_REQUEST,  # Use 400 Bad Request if the serializer validation fails
         )
-    
+
     # Handle GET request for fetching the list of Items
     def get(self, request):
         queryset = SubItem.objects.all().filter(is_deleted=False)
@@ -252,9 +253,9 @@ class SubItemViewSet(generics.GenericAPIView):
             },
             status=status.HTTP_200_OK,
         )
-    
-    def delete(self, request,pk):
-    
+
+    def delete(self, request, pk):
+
         try:
             admin = SubItem.objects.get(id=pk, is_deleted=False)
             admin.is_deleted = True
