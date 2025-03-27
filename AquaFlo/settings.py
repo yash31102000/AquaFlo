81577b2290c7d11f50aa8cfb18b9ffa3550b277c
chiragh999 <chiraghirapra999@gmail.com>
2025-03-27 14:49:51 +0530

    changes

--- AquaFlo/settings.py
+++ AquaFlo/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "rest_framework_simplejwt",
     "user",
     "category",
