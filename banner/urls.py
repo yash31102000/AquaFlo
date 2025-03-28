5377be425e114a32484990ee2a5ec17762520d7b
Yash <yash31102000.s@gmail.com>
2025-03-28 15:08:57 +0530

    changes

--- banner/urls.py
+++ banner/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("add-banner/", BannerViewSet.as_view()),
-    path("list-banner/", BannerViewSet.as_view()),
-    path("delete-banner/", BannerViewSet.as_view()),
+    path("banner/", BannerViewSet.as_view()),
 ]
