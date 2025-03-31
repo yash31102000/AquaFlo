d94f331aaedc521dd65d43f2851a2b902a191bf8
chiragh999 <chiraghirapra999@gmail.com>
2025-03-31 09:57:58 +0530

    changes

--- category/urls.py
+++ category/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("category/", CategoryViewSet.as_view()),
-    path("category/<int:pk>", CategoryViewSet.as_view()),
-    path("sub-category/", ItemViewSet.as_view()),
-    path("sub-category/<int:pk>", CategoryViewSet.as_view()),
-    path("items/", SubItemViewSet.as_view()),
-    path("items/<int:pk>", CategoryViewSet.as_view()),
-    path("watertank/",WatertankViewSet.as_view()),
-    path("watertank/<int:pk>",WatertankViewSet.as_view()),
+    # path("category/", CategoryViewSet.as_view()),
+    # path("category/<int:pk>", CategoryViewSet.as_view()),
+    # path("sub-category/", ItemViewSet.as_view()),
+    # path("sub-category/<int:pk>", CategoryViewSet.as_view()),
+    # path("items/", SubItemViewSet.as_view()),
+    # path("items/<int:pk>", CategoryViewSet.as_view()),
+    # path("watertank/",WatertankViewSet.as_view()),
+    # path("watertank/<int:pk>",WatertankViewSet.as_view()),
     path('pipes/', PipeViewSet.as_view(), name='pipe-list'),
     path('pipes/<int:pk>/', PipeViewSet.as_view(), name='pipe-detail'),
 ]
