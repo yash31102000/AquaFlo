024b7e21026d47aed4964c2a0d239e9aa169d750
chiragh999 <chiraghirapra999@gmail.com>
2025-03-31 11:16:01 +0530

    changes

--- category/serializers.py
+++ category/serializers.py
@@ -1,40 +1,6 @@
 from rest_framework import serializers
 from .models import *
 
-
-class SubItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubItem
-        fields = ["id", "name", "item", "uniqcode", "image"]
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    sub_category = serializers.CharField(source="name")
-    sub_items = SubItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Item
-        fields = ["id", "sub_category", "category", "sub_items"]
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    main_category = serializers.CharField(source="name")
-    items = ItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Category
-        fields = [
-            "id",
-            "main_category",
-            "items",
-        ]
-
-class WaterTankSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Watertank
-        fields = ["id","sub_item","height","width"]
-
 class RecursivePipeSerializer(serializers.ModelSerializer):
     """
     Recursive serializer that allows for nested sub-categories of unlimited depth.
