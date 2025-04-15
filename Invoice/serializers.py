08022932b2c9317ec08e917d80cc728b5a69ec89
Yash <yash31102000.s@gmail.com>
2025-04-15 12:33:49 +0530

    changes

--- Invoice/serializers.py
+++ Invoice/serializers.py
@@ -16,19 +16,3 @@ class InvoiceSerializer(serializers.ModelSerializer):
             "payment_status",
             "payment_method",
         ]
-
-    def validate(self, data):
-        """
-        Custom validation for the invoice total amount and final amount.
-        """
-        total_amount = data.get("total_amount")
-        tax_amount = data.get("tax_amount", Decimal("0"))
-        discount = data.get("discount", Decimal("0"))
-
-        # Final amount should be the sum of total amount + tax - discount + shipping fee
-        calculated_final_amount = total_amount + tax_amount - discount
-        if data.get("final_amount") != calculated_final_amount:
-            raise serializers.ValidationError(
-                "Final amount is incorrect. It must be the sum of total amount, tax, shipping fee, and discount."
-            )
-        return data
