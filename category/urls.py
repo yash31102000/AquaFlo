3368d28d4597e1c606acf4ea8afa2fd80a1159da
chiragh999 <chiraghirapra999@gmail.com>
2025-04-21 15:08:01 +0530

    changes

--- category/urls.py
+++ category/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path("pipes/", PipeViewSet.as_view()),
     path("pipes/<int:pk>/", PipeViewSet.as_view()),
-    path('pipes/', PipeViewSet.as_view()),
-    path('get-product-list/<int:pk>/', GetPipeViewset.as_view()),
-    path('bestseller/', BestSellerViewset.as_view()),
+    path("pipes/", PipeViewSet.as_view()),
+    path("get-product-list/<int:pk>/", GetPipeViewset.as_view()),
+    path("bestseller/", BestSellerViewset.as_view()),
     path("bestseller/<int:pk>/", BestSellerViewset.as_view()),
-    path("main-category/",GetMainCategoryViewset.as_view())
+    path("main-category/", GetMainCategoryViewset.as_view()),
 ]
--- category/views.py
+++ category/views.py
@@ -115,6 +115,7 @@ class PipeViewSet(DefaultResponseMixin, generics.GenericAPIView):
         except Exception as e:
             return self.error_response(f"Pipe update failed: {str(e)}")
 
+
 class GetPipeViewset(DefaultResponseMixin, generics.GenericAPIView):
     def update_image_urls(self, base_url, data):
         if isinstance(data, list):
@@ -130,52 +131,57 @@ class GetPipeViewset(DefaultResponseMixin, generics.GenericAPIView):
     def get(self, request, pk=None):
         try:
             main_pip = Pipe.objects.filter(pk=pk).values().first()
-            sub_categories = Pipe.objects.filter(parent = main_pip.get("id")).values()
+            sub_categories = Pipe.objects.filter(parent=main_pip.get("id")).values()
             related_product = []
             sub_categories_name_list = []
             if sub_categories.exists():
                 for sub_categorie in sub_categories:
                     sub_categories_name_list.append(sub_categorie.get("name"))
-                    products = Pipe.objects.filter(product = sub_categorie.get("id")).values()
+                    products = Pipe.objects.filter(
+                        product=sub_categorie.get("id")
+                    ).values()
                     for product in products:
-                        base_url = request.build_absolute_uri("/").rstrip("/") + "/media/"
+                        base_url = (
+                            request.build_absolute_uri("/").rstrip("/") + "/media/"
+                        )
                         self.update_image_urls(base_url, product)
                         product["sub_categorie_name"] = sub_categorie.get("name")
                         related_product.append(product)
-            products = Pipe.objects.filter(product = main_pip.get("id")).values()
+            products = Pipe.objects.filter(product=main_pip.get("id")).values()
             for product in products:
                 base_url = request.build_absolute_uri("/").rstrip("/") + "/media/"
                 self.update_image_urls(base_url, product)
                 product["sub_categorie_name"] = ""
                 related_product.append(product)
-            response_data = {"filtter_list":sub_categories_name_list,"related_product_list":related_product}
+            response_data = {
+                "filtter_list": sub_categories_name_list,
+                "related_product_list": related_product,
+            }
             return self.success_response(
-                        "Products List Fatch successfully",response_data
-                    )
+                "Products List Fatch successfully", response_data
+            )
         except Exception as e:
             return self.error_response("Product List Not Fatch")
-        
+
 
 class BestSellerViewset(DefaultResponseMixin, generics.GenericAPIView):
 
-    def post(self,request):
+    def post(self, request):
         serializer = BestSellerSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return self.success_response("BestSeller Placed successfully")
         return self.error_response("BestSeller Placed Faild")
-    
 
-    def put(self,request,pk):
+    def put(self, request, pk):
         queryset = BestSeller.objects.filter(id=pk).first()
         if not queryset:
             return self.error_response("BestSeller Not Found")
-        serializer = BestSellerSerializer(queryset,data=request.data,partial=True)
+        serializer = BestSellerSerializer(queryset, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return self.success_response("BestSeller Update successfully")
         return self.error_response("BestSeller Update Faild")
-
 
     def get(self, request):
         # Step 1: Aggregate quantities from order_items
@@ -198,36 +204,46 @@ class BestSellerViewset(DefaultResponseMixin, generics.GenericAPIView):
                 bs.total_order_quantity = product_quantities.get(bs.id, 0)
 
             # Step 3: Sort by total quantity
-            sort_best_sellers = sorted(best_sellers_product, key=lambda x: x.total_order_quantity, reverse=True)
+            sort_best_sellers = sorted(
+                best_sellers_product, key=lambda x: x.total_order_quantity, reverse=True
+            )
 
             # Step 4: Limit to BestSeller.quantity
-            top_n = best_seller.quantity # default to 5 if not set
+            top_n = best_seller.quantity  # default to 5 if not set
             sorted_best_sellers = sort_best_sellers[:top_n]
-            
+
             base_url = request.build_absolute_uri("/").rstrip("/")
-            
+
             # Step 5: Serialize and return
             serializer = PipeSerializer(sorted_best_sellers, many=True)
             for serializer_data in serializer.data:
                 serializer_data["image"] = base_url + serializer_data.get("image")
-            return self.success_response("BestSeller Fetched Successfully",serializer.data)
+            return self.success_response(
+                "BestSeller Fetched Successfully", serializer.data
+            )
         return self.success_response("BestSeller Fetched Successfully")
-    
 
-    def delete(self,request,pk):
+    def delete(self, request, pk):
         best_seller = BestSeller.objects.filter(id=pk).filter()
         if not best_seller:
             return self.error_response("BestSeller Not Found")
-        
+
         best_seller.delete()
         return self.success_response("BestSeller Delete Successfully")
-    
+
+
 class GetMainCategoryViewset(DefaultResponseMixin, generics.GenericAPIView):
 
-    def get(self,request):
+    def get(self, request):
         try:
-            main_category = Pipe.objects.filter(parent__isnull=True, product__isnull=True).values()
-            serializer = PipeSerializer(main_category, many=True)
-            return self.success_response("Main Category Fetched Successfully",serializer.data)
+            main_category = Pipe.objects.filter(
+                parent__isnull=True, product__isnull=True
+            ).values("id", "name", "image")
+            base_url = request.build_absolute_uri("/").rstrip("/") + "/media/"
+            for category in main_category:
+                category["image"] = base_url + category.get("image")
+            return self.success_response(
+                "Main Category Fetched Successfully", main_category
+            )
         except:
             return self.error_response("Main Category Not Fetched")
