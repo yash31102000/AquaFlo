1f4f38bfea2039e155192dda8ebadce418fa69ed
yash31102000 <yash31102000.s@gmail.com>
2025-05-02 16:47:38 +0530

    changes

--- user/views.py
+++ user/views.py
@@ -132,7 +132,7 @@ class AddorRemoveAddressAPI(DefaultResponseMixin, generics.GenericAPIView):
 
         get_address.save()
         return self.success_response(
-            "Address Update or Delete Successfully", get_address.addresses
+            "Address Update or Delete Successfully", 
         )
 
 class UserDiscountViewSet(DefaultResponseMixin, generics.GenericAPIView):
