2951ca4d9c14a10fd5e9ffe535884a5fc76704a6
yash31102000 <yash31102000.s@gmail.com>
2025-05-05 12:30:25 +0530

    changes

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,5 @@ from .views import *
 urlpatterns = [
     path("order/", OrderViewSet.as_view()),
     path("order/<int:pk>/", OrderViewSet.as_view()),
-    path("order/<str:user_id>/", UserOrderViewSet.as_view()),
     path("order-split/", OrderSplitViewSet.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -24,122 +24,39 @@ class OrderViewSet(DefaultResponseMixin, generics.GenericAPIView):
             return self.success_response("Order placed successfully")
         return self.error_response("Order Placed Faild")
 
-    def get(self, request):
-        queryset = Order.objects.all().order_by("created_at")
-        serializer = OrderSerializer(queryset, many=True)
-        response_data = serializer.data.copy()
-        for data in response_data:
-            data["created_at"] = (
-                datetime.fromisoformat(data["created_at"].replace("Z", ""))
-                .date()
-                .strftime("%d-%m-%Y")
-            )
-            for order_items in data.get("order_items"):
-                sub_item = (
-                    Pipe.objects.filter(pk=order_items.get("item_id"))
-                    .select_related("product")
-                    .first()
-                )
-                basic_data = (
-                    PipeDetail.objects.filter(pipe=order_items.get("item_id"))
-                    .values("basic_data")
-                    .first()
-                )
-                for basic in basic_data.get("basic_data"):
-                    if order_items.get("code") == basic.get("code") and order_items.get(
-                        "mm"
-                    ) == basic.get("mm"):
-                        item_basic_data = basic
-                        value = int(
-                            (
-                                int(basic.get("packing"))
-                                * int(order_items.get("quantity"))
-                            )
-                            / int(basic.get("large_bag"))
-                        )
-                        if value != 0:
-                            order_items["quantity"] = ""
-                            order_items["large_bag_quantity"] = str(value)
-                        else:
-                            order_items["large_bag_quantity"] = str(value)
-                        order_items.pop("code")
-                        order_items.pop("mm")
-
-                if sub_item:
-                    category_value_name =   f"{sub_item.product.parent.name} >> {sub_item.product.name}"
-                    base_url = request.build_absolute_uri("/").rstrip("/")
-                    image_url = str(sub_item.image) if sub_item.id else None
-                    order_items.pop("item_id")
-                    order_items["item"] = {
-                        "id": sub_item.id,
-                        "name": sub_item.name,
-                        "image": (
-                            base_url + "/media/" + image_url if image_url else None
-                        ),
-                        "parent_id": sub_item.parent.id if sub_item.parent else None,
-                        "product_id": sub_item.product.id if sub_item.product else None,
-                        "marked_as_favorite": sub_item.marked_as_favorite,
-                        "category": category_value_name,
-                        "basic_data": item_basic_data,
-                    }
-
-        return self.success_response("Order list fetched successfully", serializer.data)
-
-    def put(self, request, pk):
-
-        order = Order.objects.filter(id=pk).first()
-
-        if not order:
-            return self.error_response("Order not found")
-
-        status = request.data.get("status")
-        if status == "CANCEL":
-            cancellation_reason = request.data.get("cancellation_reason")
-            if not cancellation_reason:
-                return self.error_response(
-                    "Cancellation reason is required when order status is CANCEL."
-                )
-        order_items = request.data.get("order_items")
-        if order_items:
-            for order_item in order_items:
-                if not order_item.get("item_id"):
-                    return self.error_response("item_id missing in order_items")
-        serializer = OrderSerializer(order, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return self.success_response("Order Update successfully")
-        return self.error_response("Order Update Faild")
-
-
-class UserOrderViewSet(DefaultResponseMixin, generics.GenericAPIView):
-    # permission_classes = [IsAuthenticated]
-    serializer_class = OrderSerializer
-
     def get(self, request, user_id=None):
         """
-        Get all orders for a specific user
+        Fetch all orders or orders for a specific user (if user_id is provided).
         """
-        if not user_id:
-            return self.error_response("User ID is required")
+        user_id = request.query_params.get("user_id", None)
+        print(user_id,'user_id')
+        if user_id:
+            queryset = Order.objects.filter(user_id=user_id)
+        else:
+            queryset = Order.objects.all()
 
-        queryset = Order.objects.filter(user_id=user_id).order_by("created_at")
+        queryset = queryset.order_by("created_at")
         serializer = OrderSerializer(queryset, many=True)
         response_data = serializer.data.copy()
 
         for data in response_data:
+            # Format created_at
             data["created_at"] = (
                 datetime.fromisoformat(data["created_at"].replace("Z", ""))
                 .date()
                 .strftime("%d-%m-%Y")
             )
+
             for order_items in data.get("order_items"):
+                item_id = order_items.get("item_id")
                 sub_item = (
-                    Pipe.objects.filter(pk=order_items.get("item_id"))
-                    .select_related("product")
+                    Pipe.objects.filter(pk=item_id)
+                    .select_related("product", "parent")
                     .first()
                 )
+
                 basic_data = (
-                    PipeDetail.objects.filter(pipe=order_items.get("item_id"))
+                    PipeDetail.objects.filter(pipe=item_id)
                     .values("basic_data")
                     .first()
                 )
@@ -165,10 +82,13 @@ class UserOrderViewSet(DefaultResponseMixin, generics.GenericAPIView):
                             break
 
                 if sub_item:
-                    category_value_name = f"{sub_item.product.parent.name} >> {sub_item.product.name}"
                     base_url = request.build_absolute_uri("/").rstrip("/")
-                    image_url = str(sub_item.image) if sub_item.id else None
-                    order_items.pop("item_id")
+                    image_url = str(sub_item.image) if sub_item.image else None
+                    category_value_name = (
+                        f"{sub_item.product.parent.name} >> {sub_item.product.name}"
+                        if sub_item.product and sub_item.product.parent else ""
+                    )
+                    order_items.pop("item_id", None)
                     order_items["item"] = {
                         "id": sub_item.id,
                         "name": sub_item.name,
@@ -182,10 +102,34 @@ class UserOrderViewSet(DefaultResponseMixin, generics.GenericAPIView):
                         "basic_data": item_basic_data,
                     }
 
-        return self.success_response(
-            f"Orders for user fetched successfully", response_data
-        )
+        label = "user" if user_id else "all"
+        return self.success_response(f"Orders for {label} fetched successfully", response_data)
 
+
+    def put(self, request, pk):
+
+        order = Order.objects.filter(id=pk).first()
+
+        if not order:
+            return self.error_response("Order not found")
+
+        status = request.data.get("status")
+        if status == "CANCEL":
+            cancellation_reason = request.data.get("cancellation_reason")
+            if not cancellation_reason:
+                return self.error_response(
+                    "Cancellation reason is required when order status is CANCEL."
+                )
+        order_items = request.data.get("order_items")
+        if order_items:
+            for order_item in order_items:
+                if not order_item.get("item_id"):
+                    return self.error_response("item_id missing in order_items")
+        serializer = OrderSerializer(order, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return self.success_response("Order Update successfully")
+        return self.error_response("Order Update Faild")
 
 
 class OrderSplitViewSet(DefaultResponseMixin, generics.GenericAPIView):
