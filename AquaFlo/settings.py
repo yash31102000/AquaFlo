1e5b5b6b2fbed4e665b176efc8463ebb42d75266
chiragh999 <chiraghirapra999@gmail.com>
2025-06-20 10:47:20 +0530

    db changes

--- AquaFlo/settings.py
+++ AquaFlo/settings.py
@@ -105,24 +105,24 @@ WSGI_APPLICATION = "AquaFlo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "aquaflo",
-        "USER": "postgres",
-        "PASSWORD": "AquaFlo@999",
-        "HOST": "82.112.236.150",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "aquaflo",
+#         "USER": "postgres",
+#         "PASSWORD": "AquaFlo@999",
+#         "HOST": "82.112.236.150",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
